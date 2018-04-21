ea1d4db11aeb224ab90604b73befe52b27894dac
toriiwataru <eagle.hokkaido2317@gmail.com>
2018-04-22 00:10:02 +0900

    library for haskell

--- Lib.hs
+++ Lib.hs
@@ -2,8 +2,6 @@ import Control.Applicative
 import Data.List
 import Control.Monad
 
-
---f :: Num a => Int -> IO [(a,a)]
 inputTuple n = map (listToTuple.map read).map words <$> getLineN n
 
 listToTuple :: [a] -> (a,a)
@@ -11,3 +9,7 @@ listToTuple [a,b] = (a,b)
     
 getLineN :: Int -> IO [String]
 getLineN n = replicateM n getLine
+
+minl,maxl :: [Int] -> Int
+minl = foldl min maxBound
+maxl = foldl max minBound
