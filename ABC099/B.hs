733e091b2ab3baef14c2bf47ea591605d09199e4
toriiwataru <eagle.hokkaido2317@gmail.com>
2018-06-12 11:26:15 +0900

    B

---
+++ ABC099/B.hs
@@ -0,0 +1,14 @@
+import Control.Applicative
+import Data.List
+import Control.Monad
+import Data.Function
+import Data.Array
+
+main = getLine >>= putStrLn.show.answer.map read.words
+
+answer [a,b] = b2 - b where
+    n = b-a
+    b2 = sum [1..n]
+
+getLineN :: Int -> IO [String]
+getLineN n = replicateM n getLine
