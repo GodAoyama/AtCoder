535fe1cb5f95fabc3bacc346a6b6018fcfaf07ce
toriiwataru <eagle.hokkaido2317@gmail.com>
2018-06-12 11:27:31 +0900

    A

--- ABC099/A.hs
+++ ABC099/A.hs
@@ -7,7 +7,3 @@ import Data.Array
 main = getLine >>= putStrLn.answer.map read.words
 
 answer [a] = if a >= 1000 then "ABD" else "ABC"
-
-
-getLineN :: Int -> IO [String]
-getLineN n = replicateM n getLine
