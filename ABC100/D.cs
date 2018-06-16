e4c75256c1b8af789237b0be0e9b3498e6ea2d86
GodAoyama <torii.wataru@ist.osaka-u.ac.jp>
2018-06-16 22:02:32 +0900

    Create D.cs

---
+++ ABC100/D.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        var nm = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+        var ans = new long[8];
+        var inp = new long[nm[0]][];
+        for (int i = 0; i < nm[0]; i++) inp[i] = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+        for (int i = 0; i < 8; i++)
+        {
+            var score = new List<long>();
+            for (int j = 0; j < nm[0]; j++)
+            {
+                long thisScore = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    if (((i >> k) & 1) == 1) thisScore += inp[j][k];
+                    else thisScore -= inp[j][k];
+                }
+                score.Add(thisScore);
+            }
+            score = score.OrderByDescending(L => L).ToList();
+            long x = 0;
+            for (int j = 0; j < nm[1]; j++) x += score[j];
+            ans[i] = x;
+        }
+        Console.WriteLine(ans.Max());
+    }
+}
